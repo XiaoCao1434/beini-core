eb2f378a58f38b968b510541d914c2f7aa51d9e4
Administrator <Administrator@lb_chen>
2018-05-17 19:41:29 +0800

    增加一些简单类的注解，比如结果VO

--- beini-core/src/main/java/com/beini/core/vo/ResultVO.java
+++ beini-core/src/main/java/com/beini/core/vo/ResultVO.java
@@ -4,20 +4,26 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * http请求返回的最外层对象
  */
+
 @Data
 public class ResultVO<T> implements Serializable {
 
     private static final long serialVersionUID = 3068837394742385883L;
 
     /** 错误码. */
+    @ApiModelProperty(value="错误码")
     private Integer code;
 
     /** 提示信息. */
+    @ApiModelProperty(value="提示信息")
     private String msg;
 
     /** 具体内容. */
+    @ApiModelProperty(value="具体内容")
     private T data;
 }
